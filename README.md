324e1fbf7536d9f91a35794e4c592e88cb45a2ff
DrowningDragons <jaredtaylor.99@gmail.com>
2021-11-10 16:32:43 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VIACFBridge
+Plugin used to bridge VaultIt and ACF together
+
+VaultIt! and ACF are both standalone plugins, however they can optionally be used together and the plugin provided here at no cost will do the integration for you
+
+[Learn how to use it here](https://github.com/DrowningDragons/VaultIt-Wiki/wiki/ACF-Integration)
---
+++ Source/VIACFBridge/Private/VIACFRiderAnimInstance.cpp
@@ -0,0 +1,86 @@
+// Copyright (c) 2019-2021 Drowning Dragons Limited. All Rights Reserved.
+
+
+#include "VIACFRiderAnimInstance.h"
+#include "Pawn/VICharacterBase.h"
+#include "VIBlueprintFunctionLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UVIACFRiderAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = (TryGetPawnOwner()) ? Cast<AVICharacterBase>(TryGetPawnOwner()) : nullptr;
+}
+
+void UVIACFRiderAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (Character)
+	{
+		bool bWasVaulting = bIsVaulting;
+		bIsVaulting = Character->IsVaulting();
+		if (bIsVaulting)
+		{
+			// Resetting these while vaulting leads to better blending out
+			Speed = 0.f;
+
+			// Interp FBIK
+			UVIBlueprintFunctionLibrary::InterpolateFBIK(DeltaTime, FBIK);
+
+			// Right Hand
+			{
+				const FVIBoneFBIKData* BoneData = UVIBlueprintFunctionLibrary::GetBoneForFBIK(RHandName, FBIK);
+				bRHand = BoneData->bEnabled;
+				RHandLoc = BoneData->Location;
+			}
+			// Left Hand
+			{
+				const FVIBoneFBIKData* BoneData = UVIBlueprintFunctionLibrary::GetBoneForFBIK(LHandName, FBIK);
+				bLHand = BoneData->bEnabled;
+				LHandLoc = BoneData->Location;
+			}
+			// Both Hands
+			{
+				bBothHand = (bRHand && bLHand);
+				if (bBothHand)
+				{
+					// Use only control rig with both IK
+					// Only ever uses one control rig at a time
+					bRHand = false;
+					bLHand = false;
+				}
+			}
+		}
+
+		if (bIsVaulting && !bWasVaulting)
+		{
+			OnStartVault();
+		}
+		else if (bWasVaulting && !bIsVaulting)
+		{
+			OnStopVault();
+		}
+	}
+}
+
+void UVIACFRiderAnimInstance::OnStartVault()
+{
+	K2_OnStartVault();
+}
+
+void UVIACFRiderAnimInstance::OnStopVault()
+{
+	// Stopped vaulting
+	bRHand = false;
+	bLHand = false;
+	bBothHand = false;
+
+	K2_OnStopVault();
+}
+
+void UVIACFRiderAnimInstance::SetBoneFBIK_Implementation(const FName& BoneName, const FVector& BoneLocation, bool bEnabled)
+{
+	UVIBlueprintFunctionLibrary::ToggleBoneFBIK(BoneName, BoneLocation, bEnabled, FBIK);
+}
